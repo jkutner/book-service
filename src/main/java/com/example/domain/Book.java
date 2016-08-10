82f1ccfff7e0cf5d7abb23492f4755112945c6c6
Joe Kutner <jpkutner@gmail.com>
2016-08-10 09:22:41 -0500

    first

---
+++ src/main/java/com/example/domain/Book.java
@@ -0,0 +1,67 @@
+package com.example.domain;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Book.
+ */
+@Entity
+@Table(name = "book")
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        if(book.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+            "id=" + id +
+            ", isbn='" + isbn + "'" +
+            '}';
+    }
+}
